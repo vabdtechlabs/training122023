007307c0f75032338dbd81867d15287f323f95ac
POTTIR06 <ranibhavanitata@gmail.com>
2024-01-03 15:08:36 +0530

    basic oops in c#

---
+++ rani/parametarized constructor.cs
@@ -0,0 +1,22 @@
+﻿class con
+{
+
+    String name;
+    int id;
+
+    
+    con(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+   
+    public static void Main()
+    {
+
+       
+        con con1 = new con("me", 1);
+        Console.WriteLine("conName = " + con1.name +
+                         " and conId = " + con1.id);
+    }
+}
